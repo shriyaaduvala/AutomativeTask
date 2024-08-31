362bea2b8572149fac047f8c15e56fee948d614f
shriyaaduvala <shriyaaduvala@gmail.com>
2024-08-31 15:40:05 +0530

    Update Automativetask.py

--- Automativetask.py
+++ Automativetask.py
@@ -13,10 +13,18 @@ if response.status_code == 200:
     
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
+
+    #Find all <a> tags with the class name 'btn'
+    Register_link = soup.find_all(name="a")
+
+    # Loop through the register_links and print them
+    
+    for register in Register_link:
+        print(register.get("href"))
     
     # Find all <h2> tags with the class name 'news-title'
     news_titles = soup.select('h2', class_='span')
-    
+
     # Loop through the titles and print them
     for title in news_titles:
         print(title.get_text())
